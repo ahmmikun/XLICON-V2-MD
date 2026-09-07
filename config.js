ea09cad7f123fe0d4e2cf2650cf396c54c0f4675
ABRAHAM <120536940+abrahamdw882@users.noreply.github.com>
2026-09-07 01:50:43 +0000

    Update menuImage URL in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
 global.owners = ['214302325760156@lid', '25770239992037@lid'];
 global.dev = ['233533763772@s.whatsapp.net','25770239992037@lid'];
-global.menuImage = 'https://i.ibb.co/BVmdwyv8/IMG-20260417-WA0030.jpg';
+global.menuImage = 'https://i.ibb.co/5xRGprW0/IMG-20260907-WA0020-2.jpg';
 global.ownerName = 'abztech🇬🇭';
