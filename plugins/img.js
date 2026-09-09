0ff42434ca013f755feefda55e9ceba04e447a34
Zaid Hussain <zaidbscs@gmail.com>
2026-09-09 09:35:11 +0500

    updated img.js

--- plugins/img.js
+++ plugins/img.js
@@ -9,6 +9,9 @@ module.exports = {
 
     async execute(sock, m, args) {
         try {
+            // reat emoji for this cmd
+            await m.react('🌄');
+
             if (!args[0]) {
                 return m.reply('Usage: .img <query> [count]');
             }
