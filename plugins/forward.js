4ede2d28d9fcefc0575624e3d58f5b31004065dd
Zaid Hussain <zaidbscs@gmail.com>
2026-09-09 09:08:41 +0500

    Reat Emoji Added in forward.js

--- plugins/forward.js
+++ plugins/forward.js
@@ -4,6 +4,8 @@ module.exports = {
 
     async execute(sock, m, args) {
         try {
+            // reat emoji for this cmd
+            await m.react('➡️');
             if (!m.quoted) {
                 return m.reply(" Reply to a message to forward.");
             }
