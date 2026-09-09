bdae0c8b444aaef8c544f49f75c937c016a89029
Zaid Hussain <zaidbscs@gmail.com>
2026-09-09 09:28:33 +0500

    updated create.js

--- plugins/create.js
+++ plugins/create.js
@@ -5,8 +5,7 @@ module.exports = {
     aliases: ['gimg', 'gen', 'timg'],
     
     async execute(sock, m, args) {
-        // reat emoji for this cmd
-        await m.react('🎨');
+        
         if (!args.length) {
             return m.reply(`ᴛᴇxᴛ ᴛᴏ ɪᴍᴀɢᴇ\n\nᴜsᴀɢᴇ: .ɢᴇɴɪᴍɢ <ᴛᴇxᴛ>\n\nexᴀᴍᴘʟᴇ: .ɢᴇɴɪᴍɢ ᴀ ᴄᴀᴛ sɪᴛᴛɪɴɢ ᴏɴ ᴀ ᴄʜᴀɪʀ`);
         }
