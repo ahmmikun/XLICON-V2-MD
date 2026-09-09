e21d4102ddbd42ede68db891a6d5efb1b05f1e44
Zaid Hussain <zaidbscs@gmail.com>
2026-09-09 09:06:12 +0500

    Reat Emoji Added in delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -4,6 +4,8 @@ module.exports = {
     description: 'Delete a quoted message',
 
     async execute(sock, m) {
+        // reat emoji for this cmd
+        await m.react('🧹');
         if (!m.isOwner && !m.isAdmin) return
 
         if (!m.quoted) {
