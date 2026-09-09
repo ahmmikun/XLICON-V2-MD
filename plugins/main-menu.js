0fbb47977d482d80db004fac5beb7ac5cfcca582
Zaid Hussain <zaidbscs@gmail.com>
2026-09-09 09:39:09 +0500

    updated main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -6,6 +6,9 @@ module.exports = {
     aliases: ['help', 'cmdlist', 'commands'],
 
     async execute(sock, m) {
+        // reat emoji for this cmd
+            await m.react('💯');
+            
         const prefix = global.BOT_PREFIX || '.';
 
         const now = new Date();
