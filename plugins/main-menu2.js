2f05075b4f40423c24683c823cbd3ab3b3d25439
Zaid Hussain <137680240+zaidbscs@users.noreply.github.com>
2026-09-11 15:53:19 +0500

    updated

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -7,7 +7,7 @@ module.exports = {
 
     async execute(sock, m) {
         try {
-             // reat emoji for this cmd
+             
             await m.react('💯');
             await sendInteractiveMessage(sock, m.from, {
                 title: 'XLICON v2 ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
