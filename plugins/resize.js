493a46405571e4d4b6b1a61329d73173b8fdeb59
Zaid Hussain <137680240+zaidbscs@users.noreply.github.com>
2026-09-11 15:57:14 +0500

    updated

--- plugins/resize.js
+++ plugins/resize.js
@@ -9,7 +9,7 @@ module.exports = {
     command: /^.?(resize|imgresize|resizeimg)/i,
 
     async execute(sock, m, args) {
-         // reat emoji for this cmd
+       
             
         await m.react('✂️');
         const chatId = m.key.remoteJid;
