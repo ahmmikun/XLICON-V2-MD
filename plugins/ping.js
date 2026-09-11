f40808f260e48fd043d65216f4c4a64b2cd1f385
Zaid Hussain <137680240+zaidbscs@users.noreply.github.com>
2026-09-11 15:54:19 +0500

    updated

--- plugins/ping.js
+++ plugins/ping.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Check bot response speed',
 
     async execute(sock, m, args) {
-         // reat emoji for this cmd
+        
             await m.react('⏱️');
         const start = Date.now();
         const sentMsg = await m.reply('Pinging...');
