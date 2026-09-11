e98b55cc38501a85f829980eb38c8111cd4189bc
Zaid Hussain <137680240+zaidbscs@users.noreply.github.com>
2026-09-11 15:42:48 +0500

    updated creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -7,7 +7,7 @@ module.exports = {
 
     async execute(sock, m) {
         try {
-            // reat emoji for this cmd
+             
             await m.react('👑');
             const owners = [
                 ['233533763772@s.whatsapp.net', 'Abraham']
